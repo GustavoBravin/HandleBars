01f3ef18c12ece8e767c585ecea1e2b9226fb12f
GustavoBravin <gugarbravin@gmail.com>
2024-06-05 08:54:24 -0300

    rota criada
    Eu criei uma rota jogo/novo para cadastrar jogo e apertei shift + alt + f para arrumar o código e tambem criei o form

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ const conn = require("./db/conn");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const Usuario = require("./models/Usuario");
-const{where} = require("sequelizer")
+const { where } = require("sequelizer")
 const app = express();
 
 app.engine("handlebars", exphbs.engine());
-app.set("view engine","handlebars");
+app.set("view engine", "handlebars");
 
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-res.render("home")
+app.get("/", (req, res) => {
+    res.render("home")
 })
 
-app.get("/usuarios", async (req, res)=>{
-    const usuarios = await Usuario.findAll({raw: true});
+app.get("/usuarios", async (req, res) => {
+    const usuarios = await Usuario.findAll({ raw: true });
     res.render("usuarios", { usuarios });
 });
 
-app.get("/usuarios/novo",(req,res)=>{
-res.render("formUsuario")
+app.get("/usuarios/novo", (req, res) => {
+    res.render("formUsuario")
 })
 
 app.post("/usuarios/novo", async (req, res) => {
     try {
-        const {nickname,nome} = req.body;
-      
-        
+        const { nickname, nome } = req.body;
+
+
         const dadosUsuario = {
             nickname,
             nome,
@@ -43,27 +43,53 @@ app.post("/usuarios/novo", async (req, res) => {
     }
 });
 
-app.get("/usuarios/:id/uptade", async (req,res)=>{
-const id = parseInt(req.params.id)
-const usuario = await Usuario.findByPk(id,{raw: true})
+app.get("/jogos/novo", (req, res) => {
+    res.render("formJogo");
+});
+
+app.post("/jogos/novo", async (req, res) => {
+
+    try {
+        const { titulo, descricao, preco } = req.body;
+
+        const dadosJogo = {
+            titulo,
+            descricao,
+            preco,
+        };
+
+        const jogo = await Jogo.create(dadosJogo);
+        console.log(jogo)
+
+        res.send("Jogo inserido sob o id " + jogo.id);
+    } catch (error) {
+        console.error("Erro ao inserir jogo:", error);
+        res.status(500).send("Erro ao inserir jogo");
+    }
+
+});
+
+app.get("/usuarios/:id/uptade", async (req, res) => {
+    const id = parseInt(req.params.id)
+    const usuario = await Usuario.findByPk(id, { raw: true })
 
 })
 
-app.post("/usuarios/:id/update", async (req, res)=> {
+app.post("/usuarios/:id/update", async (req, res) => {
     const id = parseInt(req.params.id)
     const dadosUsuario = {
         nickname: req.body.nickname,
         nome: req.body.nome,
     };
 
-    const retorno = await Usuario.update({dadosUsuario,  where: {id:id} })
-    if(retorno>0){
+    const retorno = await Usuario.update({ dadosUsuario, where: { id: id } })
+    if (retorno > 0) {
         res.redirect("/usuarios")
-    }else{
+    } else {
         res.send("Erro ao atualizar user :(")
     }
 });
-app.listen(8000,()=>{
-console.log("Servidor rodando!")
+app.listen(8000, () => {
+    console.log("Servidor rodando!")
 })
 
